396284fbb245d441d2edb991ccf0066725ff2f42
ENUGANTI KARTHIK <enugantikarthikyadav@gmail.com>
2026-04-18 17:13:52 +0530

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,5 +1,10 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../serviceAccountKey.json");
+
+if (!process.env.FIREBASE_KEY) {
+  throw new Error("FIREBASE_KEY is missing in environment variables");
+}
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
