c86a7c5981f6231dcd41fb7fcaf63a747037704a
ENUGANTI KARTHIK <enugantikarthikyadav@gmail.com>
2026-04-18 22:53:33 +0530

    Update notificationRoutes.js

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -3,6 +3,20 @@ const router = express.Router();
 
 const notificationController = require("../controllers/notificationController");
 
+let tokens = [];
+
+router.post("/save-token", (req, res) => {
+  const { token } = req.body;
+
+  if (!tokens.includes(token)) {
+    tokens.push(token);
+  }
+
+  console.log("TOKENS:", tokens);
+
+  res.json({ success: true });
+});
+
 router.post("/send", notificationController.send);
 
-module.exports = router;
+module.exports = { router, tokens };
