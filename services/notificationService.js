c9328fe7fae037a095a31571ddfc711e8f2b2866
ENUGANTI KARTHIK <enugantikarthikyadav@gmail.com>
2026-04-19 12:16:09 +0530

    Update notificationService.js

--- services/notificationService.js
+++ services/notificationService.js
@@ -1,15 +1,25 @@
 const admin = require("../config/firebase");
+const { tokens } = require("../routes/notificationRoutes");
 
 const sendNotification = async (title, body) => {
-  const message = {
-    notification: {
-      title,
-      body,
-    },
-    topic: "news",
-  };
+  try {
+    for (let token of tokens) {
+      const message = {
+        token: token,   
+        notification: {
+          title,
+          body,
+        },
+      };
 
-  return await admin.messaging().send(message);
+      await admin.messaging().send(message);
+    }
+
+    console.log("Notification sent to all users");
+
+  } catch (err) {
+    console.error("Notification error:", err);
+  }
 };
 
 module.exports = { sendNotification };
